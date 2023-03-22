fde543aa83f92e273488a8c73f5c0a142a5dc702
Pierrot <pierre.bournet@sncf.fr>
2023-03-22 14:00:02 +0100

    fix footer responsive + icones

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,103 +1,45 @@
-import "./styles/styles.css"
+import React from 'react';
+import './styles/styles.css';
 
-export function Footer(props: any) {
+export function Footer() {
+  return (
+    <footer className="bg-dark text-center text-white">
+      <div className="container p-4">
+        <section className="mb-4">
+          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#000000' }} href="#!" role="button">
+            <img className="footer-icon" src="/footer-images/Facebook.png" alt="Facebook" />
+          </a>
+          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#000000' }} href="#!" role="button">
+            <img className="footer-icon" src="/footer-images/Twitter.png" alt="Twitter" />
+          </a>
+          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#000000' }} href="#!" role="button">
+            <img className="footer-icon" src="/footer-images/Telegram.png" alt="Telegram" />
+          </a>
+          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#000000' }} href="#!" role="button">
+            <img className="footer-icon" src="/footer-images/Instagram.png" alt="Instagram" />
+          </a>
+          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#000000' }} href="#!" role="button">
+            <img className="footer-icon" src="/footer-images/Linkedin.png" alt="LinkedIn" />
+          </a>
+          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#000000' }} href="#!" role="button">
+            <img className="footer-icon" src="/footer-images/Whatsapp.png" alt="Whatsapp" />
+          </a>
+        </section>
 
+        <section className="mb-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            distinctio earum repellat quaerat voluptatibus placeat nam,
+            commodi optio pariatur est quia magnam eum harum corrupti dicta,
+            aliquam sequi voluptate quas.
+          </p>
+        </section>
+      </div>
 
-    // Affichage
-    return (
-        <>
-            <div className="container-fluid p-3 mb-2 bg-dark bg-gradient text-white">
-
-                <div className="row justify-content-start text-center">
-                    <div className="col-4">
-                        <p className="footer">Happy Training</p>
-                    </div>
-
-
-
-                    <div className="col-2">
-                        <div className="row justify-content-start text-center">
-                            <p className="suivez_nous">Suivez-nous</p>
-                        </div>
-                        <div className="row justify-content text-center">
-                            <div className="réseaux col-1">
-                                <img src="/footer-images/facebook.png" className="réseaux" alt="icon" width="48" height="48" />
-                            </div>
-                            <div className="réseaux col-1">
-                                <img src="/footer-images/telegram.png" className="réseaux" alt="icon" width="48" height="48" />
-                            </div>
-                            <div className="réseaux col-1">
-                                <img src="/footer-images/instagram.png" className="réseaux" alt="icon" width="48" height="48" />
-                            </div>
-                            <div className="réseaux col-1">
-                                <img src="/footer-images/facebook.png" className="réseaux" alt="icon" width="48" height="48" />
-                            </div>
-                        </div>
-                        <div className="row justify-content-start text-center">
-                            <p className="suivez_nous">Tel:</p>
-                            <p className="numero">+33 607080910</p>
-                        </div>
-                    </div>
-
-
-
-                    <div className="footer-third-column col-6">
-                        <div className="footer-third-column-row row justify-content text-center">
-                            <div className="footer-right col-3">
-                                <p className="footer-right-titles">Entraînement</p>
-                                <p>
-                                    Landing Page<br />
-                                    Popup Builder<br />
-                                    Web-design<br />
-                                    Content<br />
-                                    Integrations
-                                </p>
-                            </div>
-                            <div className="footer-right col-3">
-                                <p className="footer-right-titles">Use Cases</p>
-                                <p>
-                                    Web-designers<br />
-                                    Marketers<br />
-                                    Small Business<br />
-                                    Website Builder<br />
-                                </p>
-                            </div>
-                            <div className="footer-right col-3">
-                                <p className="footer-right-titles">Company</p>
-                                <p>
-                                    About Us<br />
-                                    Careers<br />
-                                    FAQs<br />
-                                    Teams<br />
-                                    Contact Us
-                                </p>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-
-
-                <div className="row justify-content-start text-center">
-                    <div className="col-1">
-                        <p className="footer_mentions_legales">Privacy Policy</p>
-                    </div>
-                    <div className="col-1">
-                        <p className="footer_mentions_legales">Terms Of Use</p>
-                    </div>
-                    <div className="col-1">
-                        <p className="footer_mentions_legales">Sales and Refunds</p>
-                    </div>
-                    <div className="col-1">
-                        <p className="footer_mentions_legales">Legal</p>
-                    </div>
-                    <div className="col-1">
-                        <p className="footer_mentions_legales">Site Map</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-
+      <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright:
+        <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      </div>
+    </footer>
+  );
 }
